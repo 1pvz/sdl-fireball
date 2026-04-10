e38e8750c9210583f6f05244ab978cde758f5e55
1pvz <one@work.com>
2026-04-10 16:23:44 +0800

    Create 01.c

---
+++ 01.c
@@ -0,0 +1,42 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+int
+main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) == false) {
+        SDL_Log("Error initializing SDL: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    const int w = 640;
+    const int h = 480;
+    SDL_Window *window = SDL_CreateWindow("Hello SDL3", w, h, 1);
+    if (window == NULL) {
+        SDL_Log("sdl create window failed %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Surface *surface = SDL_GetWindowSurface(window);
+    if (surface == NULL) {
+        SDL_Log("Error getting surface: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    Uint32 color = SDL_MapSurfaceRGB(surface, 255, 0, 0);
+    SDL_FillSurfaceRect(surface, NULL, color);
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Event event;
+    while (SDL_WaitEvent(&event)) {
+        if (event.type == SDL_EVENT_QUIT) {
+            break;
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    window = NULL;
+    surface = NULL;
+    SDL_Quit();
+
+    return 0;
+}
