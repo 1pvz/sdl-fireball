623b109182cc67a1e1615c6dc5836be4b2a77e90
1pvz <one@work.com>
2026-04-21 14:13:16 +0800

    Update paddle.h

--- 09/paddle.h
+++ 09/paddle.h
@@ -2,13 +2,18 @@
 
 #include "gua_image.h"
 
+struct _Game;
+typedef struct _Game Game;
+
+struct _Paddle;
+typedef struct _Paddle Paddle;
 struct _Paddle {
     GuaImage image;
+    struct _Game *game;
     int speed;
-    void (*moveLeft)(struct _Paddle *paddle);
-    void (*moveRight)(struct _Paddle *paddle);
+    void (*moveLeft)(Paddle *paddle);
+    void (*moveRight)(Paddle *paddle);
 };
-typedef struct _Paddle Paddle;
 
 void
 Paddle_move(Paddle *self, int x);
